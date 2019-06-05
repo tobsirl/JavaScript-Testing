77ff933d4dc94017329fe06f71072575060d3ab3
Paul Tobin <tobster111@gmail.com>
2019-06-05 11:26:27 +0100

    Added an add function, wrote tests for adding positive numbers and negative numbers - also checks that the function receives numbers

--- LevelUpTuts-Testing/src/App.js
+++ LevelUpTuts-Testing/src/App.js
@@ -1,3 +1,6 @@
 export const hello = () => 'Hello';
 
-
+export const add = (x, y) => {
+  if (typeof x !== 'number' || typeof y !== 'number') return null;
+  return x + y;
+};
--- LevelUpTuts-Testing/src/App.test.js
+++ LevelUpTuts-Testing/src/App.test.js
@@ -1,7 +1,29 @@
-import { hello } from './App';
+import { hello, add } from './App';
 
 describe('hello', () => {
   it('should output hello', () => {
     expect(hello()).toBe('Hello');
+  });
+});
+
+describe('add', () => {
+  it('should add two numbers', () => {
+    expect(add(1, 2)).toBe(3);
+    expect(add(2, 2)).toBe(4);
+    expect(add(12, 2)).toBe(14);
+    expect(add(-2, 2)).toBe(0);
+    expect(add(-2, -2)).toBe(-4);
+  });
+
+  it('should not add strings', () => {
+    expect(add(2, '2')).toBe(null);
+  });
+
+  it('should not add objects', () => {
+    expect(add(2, {})).toBe(null);
+  });
+
+  it('should not add array', () => {
+    expect(add(2, [])).toBe(null);
   });
 });
