3e1999c467878f408078813a76c932082e26b1c2
Paul Tobin <tobster111@gmail.com>
2019-06-05 15:20:28 +0100

    Added tests for when data isnt passed

--- Academind-Testing/util.test.js
+++ Academind-Testing/util.test.js
@@ -2,7 +2,14 @@ const { generateText } = require('./util');
 
 test('should ouput name and age', () => {
   const text = generateText('Max', 29);
-  expect(text).toBe('Max (29) years old');
+  expect(text).toBe('Max (29 years old)');
+});
+
+test('should output data-less text', () => {
+  const text = generateText('', null);
+  expect(text).toBe(' (null years old)');
+  const text2 = generateText();
+  expect(text2).toBe('undefined (undefined years old)')
 });
 // describe('generateText', () => {
 //   it('should', () => {
