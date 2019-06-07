90c562bf79e716b7c732f0606ec3afa6c9e05200
Paul Tobin <tobster111@gmail.com>
2019-06-07 11:26:23 +0100

    Writing tests for Linked List class

--- LeetCode-Arrays/src/LeetCode-Arrays.js
+++ LeetCode-Arrays/src/LeetCode-Arrays.js
@@ -6,3 +6,52 @@ export const arrayPairSum = nums => {
     .filter((x, index) => index % 2 === 0)
     .reduce((a, b) => a + b);
 };
+
+export class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail = newNode;
+      this.tail.next = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  print() {
+    let array = [];
+    let current;
+    while (current) {
+      array.push(current.value);
+      current = current.next;
+    }
+    console.log(array);
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      counter++;
+      current = current.next;
+    }
+    return current;
+  }
+}
--- LeetCode-Arrays/src/LeetCode-Arrays.test.js
+++ LeetCode-Arrays/src/LeetCode-Arrays.test.js
@@ -1,6 +1,30 @@
-import { arrayPairSum } from './LeetCode-Arrays';
+import { arrayPairSum, LinkedList } from './LeetCode-Arrays';
 
 test('should return an array', () => {
-  const nums = [1,4,3,2];
-  expect(arrayPairSum(nums)).toBe(4)
+  const nums = [1, 4, 3, 2];
+  expect(arrayPairSum(nums)).toBe(4);
+});
+
+const list = new LinkedList();
+list.push('One');
+list.push('Two');
+// let result = list.print();
+// console.log(result);
+
+test('should create a LinkedList', () => {
+  expect(list.head.value).toBe('One');
+  expect(list.tail.value).toBe('Two');
+  expect(list.length).toBe(2);
+});
+
+// test('should test the method print', () => {
+//   expect(list.print()).toBe(['One', 'Two']);
+// });
+
+test('should return the value at a given index', () => {
+  const list = new LinkedList();
+  list.push('One');
+  list.push('Two');
+  const result = list.get(1)
+  expect(result).toBe('Two');
 });
