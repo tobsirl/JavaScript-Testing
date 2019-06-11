659d4aa3d9079e9a93431b488168ccca31e6f9ee
Paul Tobin <tobster111@gmail.com>
2019-06-11 14:42:35 +0100

    Using a switch statement to make adding further cases easy

--- Fizz-Buzz-TDD/src/fizzbuzz.js
+++ Fizz-Buzz-TDD/src/fizzbuzz.js
@@ -30,3 +30,26 @@
 //     console.log(output);
 //   }
 // }
+
+// * using switch
+// function FizzBuzz() {
+//   for (let i = 1; i <= 100; i++) {
+//     let output = '';
+
+//     switch (true) {
+//       case i % 3 === 0 && i % 5 === 0:
+//         output += 'FizzBuzz';
+//         break;
+//       case i % 3 === 0:
+//         output += 'Fizz';
+//         break;
+//       case i % 5 === 0:
+//         output += 'Buzz';
+//         break;
+//       default:
+//         output = i;
+//         break;
+//     }
+//     console.log(i, output);
+//   }
+// }
