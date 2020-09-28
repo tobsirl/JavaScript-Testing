64c34db9eba21aa596b7ec971441e5329e20759c
Paul Tobin <tobster111@gmail.com>
2020-09-28 15:42:55 +0100

    Updating the test for App.js

--- start-rtl/src/App.js
+++ start-rtl/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import { TodoList } from './TodoList';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="App-body">
+          <BrowserRouter>
+            <Route exact path="/" component={TodoList} />
+          </BrowserRouter>
+        </div>
         <h2>Getting started with React testing library</h2>
       </header>
     </div>
--- start-rtl/src/App.test.js
+++ start-rtl/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from './custom-render';
+import { render, screen, waitForElementToBeRemoved } from './custom-render';
 import App from './App';
 
 // !Different style of testing
@@ -10,11 +10,12 @@ import App from './App';
 // });
 
 // !use describe, it pattern
-describe('<App />', () => {
-  it('Renders <App /> component correctly', () => {
+describe("<App />", () => {
+  it("Renders <App /> component correctly", async () => {
     render(<App />);
     expect(
       screen.getByText(/Getting started with React testing library/i)
     ).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.getByText(/Fetching todos/i));
   });
 });
