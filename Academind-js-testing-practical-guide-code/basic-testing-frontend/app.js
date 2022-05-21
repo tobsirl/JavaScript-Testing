1239b9f3712123edcc743700b9a399e50dc7ecea
Paul Tobin <tobster111@gmail.com>
2022-05-21 10:15:30 +0100

    Refactoring calculateResult

--- Academind-js-testing-practical-guide-code/basic-testing-frontend/app.js
+++ Academind-js-testing-practical-guide-code/basic-testing-frontend/app.js
@@ -15,19 +15,6 @@ function formSubmitHandler(event) {
 
   let result = '';
 
-  try {
-    const numbers = [];
-    for (const numberInput of numberInputs) {
-      validateStringNotEmpty(numberInput);
-      const number = transformToNumber(numberInput);
-      validateNumber(number);
-      numbers.push(number);
-    }
-    result = add(numbers).toString();
-  } catch (error) {
-    result = error.message;
-  }
-
   let resultText = '';
 
   if (result === 'invalid') {
--- Academind-js-testing-practical-guide-code/basic-testing-frontend/src/math.js
+++ Academind-js-testing-practical-guide-code/basic-testing-frontend/src/math.js
@@ -6,3 +6,18 @@ export function add(numbers) {
   }
   return sum;
 }
+
+export function calculateResult(numberValues) {
+  try {
+    const numbers = [];
+    for (const numberInput of numberInputs) {
+      validateStringNotEmpty(numberInput);
+      const number = transformToNumber(numberInput);
+      validateNumber(number);
+      numbers.push(number);
+    }
+    result = add(numbers).toString();
+  } catch (error) {
+    result = error.message;
+  }
+}
