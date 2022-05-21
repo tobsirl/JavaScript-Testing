959b82cf964fc5e0a55fcf986a3a310616ee6e82
Paul Tobin <tobster111@gmail.com>
2022-05-21 17:37:47 +0100

    Added tests

--- Academind-js-testing-practical-guide-code/basic-testing-frontend/src/output.test.js
+++ Academind-js-testing-practical-guide-code/basic-testing-frontend/src/output.test.js
@@ -1,2 +1,18 @@
 import { it, expect, describe } from 'vitest';
 import { generateResultText } from './output';
+
+describe('generateResultText', () => {
+  it('should return a string, no matter which value is passed in', () => {
+    const inputNumber = 1;
+    const inputString = 'invalid';
+    const inputBool = false;
+
+    const result1 = generateResultText(inputNumber);
+    const result2 = generateResultText(inputString);
+    const result3 = generateResultText(inputBool);
+
+    expect(result1).toBeTypeOf('string');
+    expect(result2).toBeTypeOf('string');
+    expect(result3).toBeTypeOf('string');
+  });
+});
