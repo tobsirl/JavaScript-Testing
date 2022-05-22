37769ff0651825d319c1de010f35c0cde59af2aa
Paul Tobin <tobster111@gmail.com>
2022-05-22 11:36:38 +0100

    test isn't actually working

--- Academind-js-testing-practical-guide-code/03-async-code-starting-project/async/async-example.test.js
+++ Academind-js-testing-practical-guide-code/03-async-code-starting-project/async/async-example.test.js
@@ -1,1 +1,11 @@
 import { it } from 'vitest';
+import { generateToken } from './async-example';
+
+it('should generate a token value', () => {
+  const testUserEmail = 'test@test.com';
+
+  generateToken(testUserEmail, (err, token) => {
+    expect(token).toBeDefined();
+    expect(token).toBe(2);
+  });
+});
