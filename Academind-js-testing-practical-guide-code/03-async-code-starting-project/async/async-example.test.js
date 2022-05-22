a5fe0a8c9faf0633ad517393db2c9384fd40fef3
Paul Tobin <tobster111@gmail.com>
2022-05-22 11:56:03 +0100

    Async code working with callbacks

--- Academind-js-testing-practical-guide-code/03-async-code-starting-project/async/async-example.test.js
+++ Academind-js-testing-practical-guide-code/03-async-code-starting-project/async/async-example.test.js
@@ -1,11 +1,15 @@
-import { it } from 'vitest';
+import { it, expect } from 'vitest';
 import { generateToken } from './async-example';
 
-it('should generate a token value', () => {
+it('should generate a token value', (done) => {
   const testUserEmail = 'test@test.com';
 
   generateToken(testUserEmail, (err, token) => {
-    expect(token).toBeDefined();
-    expect(token).toBe(2);
+    try {
+      expect(token).toBeDefined();
+      done();
+    } catch (error) {
+      done(error);
+    }
   });
 });
