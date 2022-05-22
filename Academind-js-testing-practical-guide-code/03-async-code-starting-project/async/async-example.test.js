1154a50749ed6ce01386d5eb30794064f88d375a
Paul Tobin <tobster111@gmail.com>
2022-05-22 12:03:57 +0100

    Using async await for promises

--- Academind-js-testing-practical-guide-code/03-async-code-starting-project/async/async-example.test.js
+++ Academind-js-testing-practical-guide-code/03-async-code-starting-project/async/async-example.test.js
@@ -1,5 +1,5 @@
 import { it, expect } from 'vitest';
-import { generateToken } from './async-example';
+import { generateToken, generateTokenPromise } from './async-example';
 
 it('should generate a token value', (done) => {
   const testUserEmail = 'test@test.com';
@@ -12,4 +12,12 @@ it('should generate a token value', (done) => {
       done(error);
     }
   });
+});
+
+it('should generate a token value (Promises)', async () => {
+  const testUserEmail = 'test@test.com';
+
+  const token = await generateTokenPromise(testUserEmail);
+
+  expect(token).toBeDefined();
 });
