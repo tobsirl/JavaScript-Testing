4fd06b37ace1d4e635157aad593ddceeb4ad28ad
Paul Tobin <tobster111@gmail.com>
2022-05-24 18:55:07 +0100

    Test working

---
+++ Academind-js-testing-practical-guide-code/01-starting-project/src/util/io.test.js
@@ -0,0 +1,8 @@
+import { it, expect } from 'vitest';
+import writeData from './io';
+
+it('should execute the writeFile method', () => {
+  const testData = 'Test';
+  const testFileName = 'text.txt';
+  return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+});
