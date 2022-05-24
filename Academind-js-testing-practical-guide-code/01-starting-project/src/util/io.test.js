432911b37cbfa0358388e07433a7ea2679518c1c
Paul Tobin <tobster111@gmail.com>
2022-05-24 19:05:29 +0100

    vi.fn

--- Academind-js-testing-practical-guide-code/01-starting-project/src/util/io.test.js
+++ Academind-js-testing-practical-guide-code/01-starting-project/src/util/io.test.js
@@ -4,5 +4,6 @@ import writeData from './io';
 it('should execute the writeFile method', () => {
   const testData = 'Test';
   const testFileName = 'text.txt';
+
   return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
 });
