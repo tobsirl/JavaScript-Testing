3eef823840d859cf3b2105351816bdc55e13a4c3
Paul Tobin <tobster111@gmail.com>
2022-05-26 19:07:24 +0100

    mockImplementation

--- Academind-js-testing-practical-guide-code/01-starting-project/src/data.test.js
+++ Academind-js-testing-practical-guide-code/01-starting-project/src/data.test.js
@@ -5,6 +5,8 @@ describe('generateReportData()', () => {
   it('should execute logFn if provided', () => {
     const logger = vi.fn(() => {});
 
+    logger.mockImplementation(() => {});
+
     generateReportData(logger);
 
     expect(logger).toBeCalled();
