b57d303f7b0f5293a264abfd19803073bd165c76
Paul Tobin <tobster111@gmail.com>
2022-05-27 17:22:59 +0100

    Tests for ValidationError

--- Academind-js-testing-practical-guide-code/02-starting-project-advanced-mocking/util/validation.test.js
+++ Academind-js-testing-practical-guide-code/02-starting-project-advanced-mocking/util/validation.test.js
@@ -8,3 +8,11 @@ it('should throw an error if an empty string is provided as a value', () => {
 
   expect(validationFn).toThrowError();
 });
+
+it('should throw an error if an empty string is provided as a value', () => {
+  const testInput = '   ';
+
+  const validationFn = () => validateNotEmpty(testInput);
+
+  expect(validationFn).toThrowError();
+});
