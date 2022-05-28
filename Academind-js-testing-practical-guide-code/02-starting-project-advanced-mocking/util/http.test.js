645586648b63a856b17156a49a8814852f0a8901
Paul Tobin <tobster111@gmail.com>
2022-05-28 09:46:01 +0100

    Using a global stub

--- Academind-js-testing-practical-guide-code/02-starting-project-advanced-mocking/util/http.test.js
+++ Academind-js-testing-practical-guide-code/02-starting-project-advanced-mocking/util/http.test.js
@@ -1,6 +1,9 @@
-import { describe, it, expect } from 'vitest';
+import { it, expect, vi } from 'vitest';
 import { sendDataRequest } from './http';
 
+const testFetch = vi.fn();
+
+vi.stubGlobal('fetch', testFetch);
 it('should return any available response data', () => {
-  sendDataRequest
+  sendDataRequest();
 });
