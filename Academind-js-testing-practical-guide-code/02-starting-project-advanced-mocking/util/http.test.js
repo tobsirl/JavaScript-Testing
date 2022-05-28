15eae59e9673b9d9e3db3916bbc747648a98ecba
Paul Tobin <tobster111@gmail.com>
2022-05-28 09:57:41 +0100

    Replacement fetch function

--- Academind-js-testing-practical-guide-code/02-starting-project-advanced-mocking/util/http.test.js
+++ Academind-js-testing-practical-guide-code/02-starting-project-advanced-mocking/util/http.test.js
@@ -1,7 +1,21 @@
 import { it, expect, vi } from 'vitest';
 import { sendDataRequest } from './http';
 
-const testFetch = vi.fn();
+const testResponseData = { testKey: 'testData' };
+
+const testFetch = vi.fn((url, options) => {
+  return new Promise((resolve, reject) => {
+    const testResponse = {
+      ok: true,
+      json() {
+        return new Promise((resolve, reject) => {
+          resolve(testResponseData);
+        });
+      },
+    };
+    resolve();
+  });
+});
 
 vi.stubGlobal('fetch', testFetch);
 it('should return any available response data', () => {
